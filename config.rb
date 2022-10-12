004274ae3299e111d4dfbc963c27bd7f86da9724
matteo1986 <m.grifantini@am-linkweb.it>
2022-10-12 14:14:33 +0200

    Add files via upload

---
+++ config.rb
@@ -0,0 +1,25 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "C:/Matteo/Doc-Matteo/Template/fidal/html"
+css_dir = ""
+sass_dir = ""
+images_dir = "images"
+generated_images_path = "C:/Matteo/Doc-Matteo/Template/fidal/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass  scss && rm -rf sass && mv scss sass
